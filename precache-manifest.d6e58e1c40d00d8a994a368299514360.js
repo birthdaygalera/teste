86f6919eda59493bbb60e4bcf3f9bc1f34a0a57a
birthdaygalera <67528968+birthdaygalera@users.noreply.github.com>
2020-06-27 23:52:58 -0300

    Add files via upload

---
+++ precache-manifest.d6e58e1c40d00d8a994a368299514360.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ea364537b7ed1f65cf12cec967c8c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f51c5f75877f8edbddd8",
+    "url": "/static/css/main.22abe870.chunk.css"
+  },
+  {
+    "revision": "d4b4b1605ae111016fb9",
+    "url": "/static/js/2.54e10524.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.54e10524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51c5f75877f8edbddd8",
+    "url": "/static/js/main.d63aeb1a.chunk.js"
+  },
+  {
+    "revision": "cea95c9bd45542184b1e",
+    "url": "/static/js/runtime-main.df480088.js"
+  },
+  {
+    "revision": "5820bf8ce58efc3a0843243a0cdf9f7e",
+    "url": "/static/media/betel.5820bf8c.png"
+  },
+  {
+    "revision": "d18df706e04a1814264df26f0fa00c17",
+    "url": "/static/media/breno-claudia.d18df706.png"
+  },
+  {
+    "revision": "e31d7badf5828f0373b77f9a9971873f",
+    "url": "/static/media/cela1.e31d7bad.jpeg"
+  },
+  {
+    "revision": "44bd4eec0d6ad4a28efebf728a6c5473",
+    "url": "/static/media/cela3.44bd4eec.jpeg"
+  },
+  {
+    "revision": "1a9f72ee66acde1d50a11212bd450b3a",
+    "url": "/static/media/flavia-lima.1a9f72ee.png"
+  },
+  {
+    "revision": "7b351634346778b7bd59b3c50e175f7f",
+    "url": "/static/media/jude2.7b351634.png"
+  },
+  {
+    "revision": "2edb7e5d9092f82758a70ba2b15bf3b2",
+    "url": "/static/media/jude3.2edb7e5d.png"
+  },
+  {
+    "revision": "da7223a8c0a5d819eebba05ebb763692",
+    "url": "/static/media/jude4.da7223a8.png"
+  },
+  {
+    "revision": "99bfc154d3458b64732bfaae56753d33",
+    "url": "/static/media/jude5.99bfc154.png"
+  },
+  {
+    "revision": "98179738e5d4f51ee97ffced3da9c643",
+    "url": "/static/media/jude6.98179738.png"
+  },
+  {
+    "revision": "7fd50b57213d7dd57cd2c63ada8afbee",
+    "url": "/static/media/jude7.7fd50b57.png"
+  },
+  {
+    "revision": "cb69381f7baf16c020c06f8985636188",
+    "url": "/static/media/paola.cb69381f.png"
+  },
+  {
+    "revision": "0e5684f1b3c2f2ea74ec4b6f0f5e8c32",
+    "url": "/static/media/pedro.0e5684f1.png"
+  },
+  {
+    "revision": "f0869584c3176589de1a9e71883c6ff2",
+    "url": "/static/media/rudy.f0869584.png"
+  },
+  {
+    "revision": "5621b7223ce007cab7a2722c2aba4323",
+    "url": "/static/media/rudy2.5621b722.jpeg"
+  },
+  {
+    "revision": "f9d0ed1c793a186598f99cda570b5a25",
+    "url": "/static/media/tammy1.f9d0ed1c.png"
+  },
+  {
+    "revision": "660f800b3c7825be1855e782be24e239",
+    "url": "/static/media/tammy2.660f800b.png"
+  },
+  {
+    "revision": "54d25389ec200190b1db710a578aef54",
+    "url": "/static/media/tammy3.54d25389.png"
+  },
+  {
+    "revision": "2b1cbd3623674a349b1ea6c016031efe",
+    "url": "/static/media/tammy4.2b1cbd36.png"
+  },
+  {
+    "revision": "a6b4258ac51678c4823847f2726db8a3",
+    "url": "/static/media/tammy5.a6b4258a.png"
+  },
+  {
+    "revision": "9667e4dfb0ec48a7a4c4696d365b23a6",
+    "url": "/static/media/tammy6.9667e4df.png"
+  },
+  {
+    "revision": "87e7dacb678586d8ee576b9be71346cf",
+    "url": "/static/media/tammy7.87e7dacb.png"
+  },
+  {
+    "revision": "92a9077f38a5f3b89cbc90ff852a164d",
+    "url": "/static/media/tammy8.92a9077f.png"
+  },
+  {
+    "revision": "167b3c203782b9778df0ea7f61422d3e",
+    "url": "/static/media/tifa.167b3c20.gif"
+  },
+  {
+    "revision": "6e693b5ea87f756a7df1e06d7fb0ad4f",
+    "url": "/static/media/zero1.6e693b5e.png"
+  },
+  {
+    "revision": "32c640c161950cd5502b189d60948575",
+    "url": "/static/media/zero2.32c640c1.png"
+  },
+  {
+    "revision": "35671807803541f69b9ee068234f11ed",
+    "url": "/static/media/zero3.35671807.png"
+  },
+  {
+    "revision": "460e73b03d1296e44eba2f13655885c3",
+    "url": "/static/media/zero4.460e73b0.png"
+  },
+  {
+    "revision": "36b2e32cf9ee1eaad32b404bf1daf271",
+    "url": "/static/media/zero5.36b2e32c.png"
+  },
+  {
+    "revision": "e62146c7888609e970b3f30a84362aa0",
+    "url": "/static/media/zero6.e62146c7.png"
+  }
+]);
